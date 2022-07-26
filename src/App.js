de55a77523d0b6792fd4938bd866193cbeb5ce77
samyyuv <samyy.uv@gmail.com>
2022-07-26 15:06:11 +0200

    creating visuals of the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
+import Numbers from './components/Numbers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  function allNewDice() {
+    let arrayNum = []
+    for (let i = 0; i < 10; i++) {
+      arrayNum.push(Math.floor((Math.random() * 6) + 1))
+    }
+    return arrayNum;
+  }
 
-      </header>
-    </div>
+  return (
+    <main>
+      <div className="tenziesContainer">
+        <h1 className="titre">Tenzies</h1>
+        <p className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="numbersContainer">
+          <Numbers
+            value="1" />
+          <Numbers
+            value="2" />
+          <Numbers
+            value="3" />
+          <Numbers
+            value="4" />
+          <Numbers
+            value="5" />
+          <Numbers
+            value="6" />
+          <Numbers
+            value="7" />
+          <Numbers
+            value="8" />
+          <Numbers
+            value="9" />
+          <Numbers
+            value="10" />
+
+        </div>
+        <div className="diceContainer">
+          <button className="dice">Roll</button>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Numbers.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Numbers(props) {
+
+  return (
+    <button className="btn-num">{props.value}</button>
+  )
+}
